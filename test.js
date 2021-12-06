21993020fc213ce75ec9ce6b925eb96a1d4f2d2f
Sonika Gupta <gupta.sonika103@gmail.com>
2021-12-06 13:38:34 +0530

    Add tests for bad outcome and perfect timing

--- test.js
+++ test.js
@@ -1,29 +1,63 @@
+const { game } = require('./globals')
 const { populateBowlTypes } = require('./populators')
 const { predictOutcome } = require('./predictOutcome')
+const shotTimings = require('./data/shotTimings.json')
+
+populateBowlTypes()
 
 describe('validateInput', () => {
   test('empty input should throw error', () => {
     expect(() => predictOutcome('')).toThrow('Invalid Input')
   })
-})
 
-describe('validateInputLength', () => {
   test('input with less than 3 parameters should throw error', () => {
     expect(() => predictOutcome('Pull Perfect')).toThrow('Invalid Input')
   })
-})
 
-describe('validateBowlType', () => {
   test('Bowl type not allowed', () => {
     expect(() => predictOutcome('BB Pull Perfect')).toThrow('Invalid Bowl Type')
   })
-})
 
-describe('validateShotType', () => {
   test('Shot type not allowed', () => {
-    populateBowlTypes()
     expect(() => predictOutcome('Bouncer SS Perfect')).toThrow(
       'Invalid Shot Type'
+    )
+  })
+})
+
+describe('bad outcome', () => {
+  const bowlType = 'Bouncer'
+  const bouncerCard = game.bowlCards.find(card => card.name === bowlType)
+  const [badShot] = Object.entries(bouncerCard.hitProbs).find(
+    ([shot, prob]) => prob < 0.4
+  )
+  console.log(badShot)
+  test('shot type with hit probability < 0.4 should return 0 runs or wicket irrespective of timing', () => {
+    shotTimings.forEach(timing => {
+      expect(predictOutcome(`${bowlType} ${badShot} ${timing}`)).toMatch(
+        /0 runs|1 wicket/
+      )
+    })
+  })
+})
+
+describe('perfect timing', () => {
+  const bowlType = 'Bouncer'
+  const bouncerCard = game.bowlCards.find(card => card.name === bowlType)
+  const [boundaryShot] = Object.entries(bouncerCard.hitProbs).find(
+    ([shot, prob]) => prob >= 0.75
+  )
+  test('shot type with hit probability >= 0.75 should result in 4 or 6 runs', () => {
+    expect(predictOutcome(`${bowlType} ${boundaryShot} Perfect`)).toMatch(
+      /4 runs|6 runs/
+    )
+  })
+  const [averageShot] = Object.entries(bouncerCard.hitProbs).find(
+    ([shot, prob]) => prob >= 0.4 && prob < 0.75
+  )
+  test('shot type with hit probability > 0.75 should result in 2 or 3 runs', () => {
+    expect(predictOutcome(`${bowlType} ${averageShot} Perfect`)).toMatch(
+      /2 runs|3 runs/
     )
   })
 })
--- utils.js
+++ utils.js
@@ -4,6 +4,7 @@ function getInputFromString (str) {
   const input = str.split(/\s/)
   console.log(input)
   if (!input || input.length !== 3) throw errors.invalidInput
+  input[2] = input[2].toLowerCase()
   return input
 }
 
