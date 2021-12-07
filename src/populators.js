62729e4304186e591707ebf9fe00d8f8d3892c55
Sonika Gupta <gupta.sonika103@gmail.com>
2021-12-07 18:33:47 +0530

    Add team populator

--- src/populators.js
+++ src/populators.js
@@ -1,15 +1,33 @@
 const bowlTypes = require('./data/bowlTypes.json')
 const shotTypes = require('./data/shotTypes.json')
-const BowlCard = require('./classes/BowlCard')
-const { game } = require('./globals')
+const teams = require('./data/teams.json')
 
-function populateBowlTypes () {
+const BowlCard = require('./classes/BowlCard')
+const Team = require('./classes/Team')
+const Player = require('./classes/Player')
+
+const { game } = require('./game')
+
+function populateBowlCards () {
   bowlTypes.forEach(type => {
     const bowlCard = new BowlCard(type, shotTypes)
     game.addBowlCard(bowlCard)
   })
 }
 
+function populateTeams () {
+  const team1 = new Team({
+    ...teams[0],
+    players: teams[0].players.map(player => new Player(player))
+  })
+  const team2 = new Team({
+    ...teams[1],
+    players: teams[1].players.map(player => new Player(player))
+  })
+  game.addTeams(team1, team2)
+}
+
 module.exports = {
-  populateBowlTypes
+  populateBowlCards,
+  populateTeams
 }
