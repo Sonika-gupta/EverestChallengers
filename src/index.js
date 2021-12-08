39b20d26fa89863bb11043dc7b3c5caf95d4e371
Sonika Gupta <gupta.sonika103@gmail.com>
2021-12-08 11:26:01 +0530

    Add REPL

--- src/index.js
+++ src/index.js
@@ -1,16 +1,60 @@
-const { predictSuperOverOutcome } = require('./predictSuperOverOutcome')
-const { populateBowlCards, populateTeams } = require('./populators')
-const { playSuperOver } = require('./predictSuperOverOutcome')
+const chalk = require('chalk')
+const boxen = require('boxen')
 
-const shotsPlayed = [
-  'Straight Perfect',
-  'Flick Early',
-  'Scoop Good',
-  'LegGlance Good',
-  'CoverDrive Late',
-  'LongOn Perfect'
-]
+const {
+  getFunctionChoice,
+  getPredictionInput,
+  getSuperOverInput,
+  getShotPlayed
+} = require('./inquiry')
+const populateGame = require('./populators')
+const { printToConsole } = require('./utils')
+const { getShotOutcome, getCommentaryOutcome } = require('./wrappers')
+const playSuperOver = require('./playSuperOver')
+const game = require('./models/game')
 
-populateBowlCards()
-populateTeams()
-playSuperOver(shotsPlayed)
+async function main () {
+  const greeting = chalk.blackBright.bold('EVEREST CHALLENGERS')
+  const boxenOptions = {
+    padding: 1,
+    margin: 1,
+    borderStyle: 'doubleSingle',
+    borderColor: 'red',
+    backgroundColor: 'white'
+  }
+  printToConsole(boxen(greeting, boxenOptions))
+
+  populateGame()
+  console.log('game:', game)
+
+  do {
+    var { functionChoice } = await getFunctionChoice()
+
+    switch (functionChoice) {
+      case 'Predict Outcome': {
+        const input = await getPredictionInput()
+        getShotOutcome(Object.values(input).join(' '))
+        break
+      }
+      case 'Get Commentary With Outcome': {
+        const input = await getPredictionInput()
+        getCommentaryOutcome(Object.values(input).join(' '))
+        break
+      }
+      case 'Play Super Over': {
+        const { chasingTeamName, target } = await getSuperOverInput()
+        const shotsPlayed = []
+        while (shotsPlayed.length != 6) {
+          const { shotType, shotTiming } = await getShotPlayed(
+            shotsPlayed.length + 1
+          )
+          shotsPlayed.push([shotType, shotTiming])
+        }
+        playSuperOver({ chasingTeamName, target, shotsPlayed })
+        break
+      }
+    }
+  } while (functionChoice !== 'Exit')
+}
+
+main()
---
+++ src/inquiry.js
@@ -0,0 +1,96 @@
+const inquirer = require('inquirer')
+
+const { bowlTypes, shotTypes, shotTimings, teams } = require('./data')
+
+function getFunctionChoice () {
+  const questions = [
+    {
+      type: 'list',
+      name: 'functionChoice',
+      message: 'Welcome to CricSummit 2021. What would you like to do?',
+      choices: [
+        'Predict Outcome',
+        'Get Commentary With Outcome',
+        'Play Super Over',
+        'Exit'
+      ],
+      default: 'Predict Outcome'
+    }
+  ]
+  return inquirer.prompt(questions)
+}
+
+function getPredictionInput () {
+  const questions = [
+    {
+      type: 'rawlist',
+      name: 'bowlType',
+      message: 'Select Bowl Type Card',
+      choices: bowlTypes,
+      default: bowlTypes[0]
+    },
+    {
+      type: 'rawlist',
+      name: 'shotType',
+      message: 'Select Shot Type Card',
+      choices: shotTypes,
+      default: shotTypes[0]
+    },
+    {
+      type: 'rawlist',
+      name: 'shotTiming',
+      message: 'Select Bowl Type Card',
+      choices: shotTimings,
+      default: shotTimings[0]
+    }
+  ]
+  return inquirer.prompt(questions)
+}
+
+function getSuperOverInput () {
+  const questions = [
+    {
+      type: 'rawlist',
+      name: 'chasingTeamName',
+      message: 'Which team is chasing?',
+      choices: teams.map(team => team.name),
+      default: teams[1].name
+    },
+    {
+      type: 'input',
+      name: 'target',
+      message: 'Enter the Target Score: ',
+      validate: function (value) {
+        if (value && !isNaN(Number(value))) return true
+        else return 'Required a numerical target score, received' + typeof value
+      }
+    }
+  ]
+  return inquirer.prompt(questions)
+}
+
+function getShotPlayed (ballNumber) {
+  const questions = [
+    {
+      type: 'rawlist',
+      name: 'shotType',
+      message: `Ball ${ballNumber}: Select Shot Type Card`,
+      choices: shotTypes,
+      default: shotTypes[0]
+    },
+    {
+      type: 'rawlist',
+      name: 'shotTiming',
+      message: `Ball ${ballNumber}: Select Bowl Type Card`,
+      choices: shotTimings,
+      default: shotTimings[0]
+    }
+  ]
+  return inquirer.prompt(questions)
+}
+module.exports = {
+  getFunctionChoice,
+  getPredictionInput,
+  getSuperOverInput,
+  getShotPlayed
+}
